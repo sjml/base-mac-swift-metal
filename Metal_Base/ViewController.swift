f22134b06ee8d64c2acdbbd829b0856157a20890
Shane Liesegang <shane@techie.net>
2017-05-13 09:14:37 -0700

    first

---
+++ Metal_Base/ViewController.swift
@@ -0,0 +1,66 @@
+import Cocoa
+import MetalKit
+
+
+class ViewController: NSViewController {
+    var device: MTLDevice!
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    
+    @IBOutlet weak var mtkView: MTKView! {
+        didSet {
+            mtkView.delegate = self
+            mtkView.preferredFramesPerSecond = 60
+            mtkView.clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        device = MTLCreateSystemDefaultDevice()
+        mtkView.device = device
+                
+        let defaultLibrary = device.newDefaultLibrary()
+        let fragmentProgram = defaultLibrary!.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary!.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        
+        commandQueue = device.makeCommandQueue()
+    }
+
+    func render(_ drawable: CAMetalDrawable?) {
+        guard let drawable = drawable else { return }
+        
+        let renderPassDescriptor = MTLRenderPassDescriptor()
+        renderPassDescriptor.colorAttachments[0].texture = drawable.texture
+        renderPassDescriptor.colorAttachments[0].loadAction = .clear
+        renderPassDescriptor.colorAttachments[0].clearColor = MTLClearColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 1.0)
+        
+        let commandBuffer = commandQueue.makeCommandBuffer()
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
+        renderEncoder.setRenderPipelineState(pipelineState)
+
+        renderEncoder.endEncoding()
+        
+        commandBuffer.present(drawable)
+        commandBuffer.commit()
+    }
+}
+
+extension ViewController: MTKViewDelegate {
+    
+    func mtkView(_ view: MTKView, drawableSizeWillChange size: CGSize) {
+        
+    }
+    
+    func draw(in view: MTKView) {
+        render(view.currentDrawable)
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Metal Base
+
+This is meant as a starting point for Swift-based Metal programs on the Mac. All it does is set up the initial device, command queue, pipeline, etc. When run, it opens a 1024x768 window rendering nothing. The basic shaders will render simple vertex buffers with every fragment white.
+
